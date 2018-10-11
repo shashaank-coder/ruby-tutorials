79418fe29c590b0f260b1dc54a6ffd721365b5d9
Sai Shashaank V V <shashithecharizardlover@gmail.com>
2018-10-11 19:44:19 +0530

    getting user input from terminal

---
+++ gettingUserInput.rb
@@ -0,0 +1,9 @@
+#user Input
+
+# you will have to run this in terminal for this to work.
+
+puts "Enter your Name : "
+name = gets.chomp(); #taking user input. chomp() function removes the "newline"
+                     #character which is taken as input if you press enter key.
+
+puts "Hello "+ name +", you are cool";
