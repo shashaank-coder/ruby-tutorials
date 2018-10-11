adc04d3483bec2f32c617959f8b4faf10db5672d
Sai Shashaank V V <shashithecharizardlover@gmail.com>
2018-10-11 16:42:43 +0530

    new file for datatypes

---
+++ datatypes.rb
@@ -0,0 +1,6 @@
+name = "Shashaank"; #string
+occupation = "software programmer"; #string
+age = 24; #can be positive or negative
+gpa = 3.2239485; #decimal/float
+is_male = true; #boolean
+flaws = nil; #has no value.
