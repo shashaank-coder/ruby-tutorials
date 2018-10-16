2901d3bfde5670ea03eb3a06c700195998b837f4
Sai Shashaank V V <shashithecharizardlover@gmail.com>
2018-10-16 19:23:08 +0530

    working with hashes, equivalent of maps

---
+++ hashes.rb
@@ -0,0 +1,10 @@
+#hashes in ruby
+
+#key-value pairs. Similar to Maps. 
+states = {
+  "Karnataka" => "Bangalore",
+  "Tamil Nadu" => "Chennai",
+  "Andhra Pradesh" => "Amaravati"
+};
+
+puts states["Karnataka"] #prints bangalore
