69173e4b2db2471c4000616eedab13eb2085a70f
Sai Shashaank V V <shashithecharizardlover@gmail.com>
2018-10-16 16:29:33 +0530

    a simple mad libs game using ruby

---
+++ madLibsGame.rb
@@ -0,0 +1,17 @@
+#a simple madlibsgame
+#input for color
+puts ("Enter a color")
+color = gets.chomp()
+
+#A plural Noun
+puts ("Enter a plural noun")
+plural_noun = gets.chomp()
+
+#Name of a Flower
+puts ("Enter a flower")
+flower = gets.chomp()
+
+#use them
+puts ("Roses are "+color)
+puts (plural_noun+" are blue")
+puts ("I love "+flower)
