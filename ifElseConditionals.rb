3f8be4d27dceddaa19d6932946a49ff2bd6d1cf5
Sai Shashaank V V <shashithecharizardlover@gmail.com>
2018-10-17 12:49:55 +0530

    playing with if else conditionals, added new content

--- ifElseConditionals.rb
+++ ifElseConditionals.rb
@@ -35,3 +35,17 @@ elsif !isMale and isTall
 else
   puts "you are neither male, nor tall"
 end
+
+#comparing and return greatest of three numbers
+
+def max(first_num,second_num,third_num)
+  if first_num > second_num and first_num > third_num #checks if first_num is greater than second_num AND third_num
+    return first_num
+  elsif second_num > first_num and second_num > third_num #checks if second_num is greater than first_num AND third_num
+    return second_num
+  else
+    return third_num #if both conditions fail, then third number is greatest
+  end
+end
+
+puts "greatest of the three numbers is = "+max(1,2,3).to_s
