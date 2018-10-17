91239ab84072ce62e854b0ebfed453a37f42adbd
Sai Shashaank V V <shashithecharizardlover@gmail.com>
2018-10-17 14:44:40 +0530

    while loops

---
+++ whileLoops.rb
@@ -0,0 +1,10 @@
+#while loops using ruby
+
+index = 1; #no of iterations
+
+while index <= 5 #loops below block for 5 times
+  puts index
+  index += 1; #same as index = index + 1;
+end
+
+#while true --> infinite loop, where condition does not become false.
